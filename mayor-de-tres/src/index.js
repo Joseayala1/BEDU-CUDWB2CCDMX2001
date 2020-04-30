6a89918ec887c190918702b51df0209c6a34f10c
jose alberto <joalcoay94@gmail.com>
2020-04-30 11:55:42 -0500

    correcion mayor-de-tres

--- mayor-de-tres/src/index.js
+++ mayor-de-tres/src/index.js
@@ -1,23 +1,17 @@
 // Prohibido usar Math.max, te estoy observando 👀
 
 function mayor_de_tres(numero1, numero2, numero3) {
-  if ((numero1 === numero2) === numero3) {
+  if (
+    numero1 >= numero2 &&
+    numero1 >= numero3 &&
+    (numero2 >= numero3 ||numero3 >= numero2)
+  ) {
     return numero1;
   } else if (
-    numero1 === Infinity ||
-    numero2 === Infinity ||
-    numero3 === Infinity
+    numero2 >= numero1 &&
+    numero2 >= numero3 &&
+    (numero1 >= numero3 || numero3 >= numero1)
   ) {
-    if (numero1 === Infinity) {
-      return numero1;
-    } else if (numero2 === Infinity) {
-      return numero2;
-    } else {
-      return numero3;
-    }
-  } else if (numero1 > numero2 > numero3) {
-    return numero1;
-  } else if (numero2 > numero1 > numero3) {
     return numero2;
   } else {
     return numero3;
