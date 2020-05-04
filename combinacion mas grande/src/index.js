4f9a45f8d4745d18804e7535c779067e520792c1
jose alberto <joalcoay94@gmail.com>
2020-05-04 12:27:23 -0500

    ejercicio combinacion mas grande resuelto

--- combinacion mas grande/src/index.js
+++ combinacion mas grande/src/index.js
@@ -1,24 +1,60 @@
-function combinacion_mas_grande(digito1, digito2, digito3) {
-  let numero = [digito1, digito2, digito3];
-  numero.sort();
-  if (numero[0] === 0 && numero[1] === 0 && numero[2] === 0) {
-    return numero[0];
-  } else if (numero[0] !== 0 && numero[1] === 0 && numero[2] === 0) {
-    return numero[0];
-  } else if (numero[0] === 0 && numero[1] !== 0 && numero[2] === 0) {
-    return numero[1];
-  } else if (numero[0] === 0 && numero[1] === 0 && numero[2] !== 0) {
-    return numero[2];
+function combinacion_mas_grande(n1, n2, n3) {
+  if (n1 === 0 && n2 === 0 && n3 === 0) {
+    return n1;
+  } else if (n1 === 0 && n2 === 0 && n3 !== 0) {
+    let com = [n3, n1, n2];
+    let com1 = com.join("");
+    let com2 = parseInt(com1);
+    return com2;
+    } else if (n1 === 0 && n2 !== 0 && n3 === 0) {
+      let com = [n2, n1, n3];
+      let com1 = com.join("");
+      let com2 = parseInt(com1);
+      return com2;
+  } else if (n1 !== 0 && n2 === 0 && n3 === 0) {
+    let com = [n1, n2, n3];
+      let com1 = com.join("");
+      let com2 = parseInt(com1);
+      return com2;
   } else {
-    var t1 = "" + numero[2] + "" + numero[1] + "" + numero[0];
-    return t1;
+    let t1;
+    if (n1 > n2) {
+      t1 = n1;
+    } else {
+      t1 = n2;
+    }
+    if (t1 > n3) {
+      t1 = t1;
+    } else {
+      t1 = n3;
+    }
+    let t2;
+    if (n2 > n1) {
+      t2 = n1;
+    } else {
+      t2 = n2;
+    }
+    if (n3 > t2) {
+      t2 = t2;
+    } else {
+      t2 = n3;
+    }
+    let t3;
+    if (t1 !== n1 && t2 !== n1) {
+      t3 = n1;
+    } else if (t1 !== n2 && t2 !== n2) {
+      t3 = n2;
+    } else {
+      t3 = n3;
+    }
+    let com = [t1, t3, t2];
+    let com1 = com.join("");
+    let com2 = parseInt(com1);
+    return com2;
   }
-
-  return null;
 }
 
-combinacion_mas_grande(0, 0, 0);
-
+console.log(combinacion_mas_grande(1, 0, 9));
 /*
   NO MOVER ESTA LÍNEA :)
   La veremos en la siguiente sesión de todos modos.
